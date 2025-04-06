524876f792c8ee18796757da0197d9055b4ebbe4
Mohammad Hasyim Taufik <55352885+hatsyim@users.noreply.github.com>
2025-04-06 07:18:51 -0500

    Update install_env.sh

--- install_env.sh
+++ install_env.sh
@@ -9,7 +9,7 @@ echo 'Creating Package environment'
 # Create conda env
 conda env create -f environment.yml
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate my_env_plot
+conda activate hcpinnseikonal
 
 # Install eikonal solver: fast-marching with Pykonal
 pip install -e git+https://github.com/malcolmw/pykonal@373a7d4#egg=pykonal
@@ -28,4 +28,3 @@ echo 'Checking cupy version and running a command...'
 python -c 'import torch; print(torch.__version__);  print(torch.cuda.get_device_name(torch.cuda.current_device())); print(torch.ones(10).to("cuda:0"))'
 
 echo 'Done!'
-
